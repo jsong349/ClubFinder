5e1ec34adfb4e2ca432fdb767865465d206f8461
vaskar <nathvask@mail.utoronto.ca>
2020-07-13 23:18:25 -0600

    finished query functionality

--- user_interface/static/main.js
+++ user_interface/static/main.js
@@ -26,22 +26,49 @@
 //     })
 // });
 
-console.log("Startin")
+function GetElementInsideContainer(containerID, childID) {
+  var elm = document.getElementById(childID);
+  var parent = elm ? elm.parentNode : {};
+  return (parent.id && parent.id === containerID) ? elm : {};
+}
+
+function duplicate(desc, img, link, title) {
+  var clone = original.cloneNode(true); // "deep" clone
+  // clone.id = "duplicater" + ++i;
+  var id = "duplicater" + ++i;
+  clone.id = id;
+  var desc =  
+  original.parentNode.appendChild(clone);
+}
 $(function() {
     $('a#process_input').bind('click', function() {
       $.getJSON('/background_process', {
         interests: $('input[name="interests"]').val(),
       }, function(data) {
-        var desc = data.result[0][0]
-        var link = data.result[0][2]
-        var img = data.result[0][1]
-        $("#desc").text(desc);
-        $("#link").prop('href', link)
-        console.log(img)
-        $("#img").prop('src', img)
-        // console.log(link)
-        // console.log(title)
-        // console.log(desc)
+
+        var result_div = document.getElementById("results"); 
+        var template = document.getElementById("template");
+        console.log(data.result)
+        for (j = 0; j < data.result.length; j++){
+          var clone = template.cloneNode(true);
+          id = 'result' + j;
+          clone.id = id;
+          clone.classList.remove('template');
+          result_div.appendChild(clone)
+          console.log(document.getElementsByClassName('desc')[1])
+          document.getElementsByClassName('desc')[1].id = 'desc' + j
+          document.getElementsByClassName('link')[1].id = 'link' + j
+          document.getElementsByClassName('img')[1].id = 'img' + j
+          document.getElementsByClassName('title')[1].id = 'title' + j
+          document.getElementsByClassName('desc')[1].classList.remove('desc')
+          document.getElementsByClassName('link')[1].classList.remove('link')
+          document.getElementsByClassName('img')[1].classList.remove('img')
+          document.getElementsByClassName('title')[1].classList.remove('title')
+          $("#desc" + j).text(data.result[j][0]);
+          $("#link" + j).prop('href', data.result[j][2])
+          $("#img" + j).prop('src', data.result[j][1])
+          $("#title" + j).text(data.result[j][3])
+        }
       });
       return false;
     });
